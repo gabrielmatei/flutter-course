7c57f8029b10f3d28045579b5b112e9f30e55d03
Gabriel Matei <gabrielvmatei@gmail.com>
2020-11-30 20:26:10 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(App());
@@ -18,18 +19,66 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isRed = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Course'),
-      ),
-      body: const Center(
-        child: Text('Hello!'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Flutter Course'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.animation),
+              onPressed: () {
+                setState(() => isRed = !isRed);
+              },
+            ),
+            Stack(
+              alignment: AlignmentDirectional.topEnd,
+              children: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.notifications),
+                  onPressed: () {},
+                ),
+                Material(
+                  shape: const CircleBorder(),
+                  elevation: 4.0,
+                  color: Colors.red,
+                  child: Container(
+                    child: const Text('13'),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+        body: Stack(
+          alignment: AlignmentDirectional.center,
+          children: <Widget>[
+            Container(
+              width: 300,
+              height: 300,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ),
+          ],
+        ));
   }
 }
